aae0cdca6074fdec93f452fa8fad21a0ca66bb1a
AtherCoder99 <a.ngaming102734@gmail.com>
2026-05-06 18:02:20 +0500

    Add files via upload

---
+++ componentA.jsx
@@ -0,0 +1,13 @@
+
+const componentA = () => {
+    let name = 'saylani';
+
+    return(
+        <div>
+            {/* <h2>{name}</h2> */}
+            <h2>This is a componentA</h2>    
+        </div>
+    )
+};
+
+export default componentA;
---
+++ componentC.jsx
@@ -0,0 +1,2 @@
+
+ import componentC from "./";
